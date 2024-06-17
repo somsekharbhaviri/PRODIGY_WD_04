141371712cc8b16d9bb532bfee34780670c905dd
2200090165@Somsekhar <143331835+somsekharbhaviri@users.noreply.github.com>
2024-06-17 19:16:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,20 +16,15 @@ function myMenuFunction(){
     const navHeader =document.getElementById("header");
 
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
-
       navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
       navHeader.style.height = "70px";
       navHeader.style.lineHeight = "70px";
-
     } else {
-
       navHeader.style.boxShadow = "none";
       navHeader.style.height = "90px";
       navHeader.style.lineHeight = "90px";
-
     }
   }
-
 
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
